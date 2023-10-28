74583790b183368501ac33395a54782f8919291b
mikhailovnikita1998 <148373103+mikhailovnikita1998@users.noreply.github.com>
2023-10-28 11:15:09 +0300

    Update Main.java

--- Calc/src/Main.java
+++ Calc/src/Main.java
@@ -1,65 +1,75 @@
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) throws Exception {
-        Scanner scn = new Scanner(System.in);
-        String exp = scn.nextLine();
-        char action;
-        String[] data;
-        if (exp.contains(" + ")) {
-            data = exp.split(" \\+ ");
-            action = '+';
-        } else if (exp.contains(" - ")) {
-            data = exp.split(" - ");
-            action = '-';
-        } else if (exp.contains(" * ")) {
-            data = exp.split(" \\* ");
-            action = '*';
-        } else if (exp.contains(" / ")) {
-            data = exp.split(" / ");
-            action = '/';
-        }else{
-            throw new Exception("Некорректный знак действия");
-        }
-        if (action == '*' || action == '/') {
-            if (data[1].contains("\"")) throw new Exception("Строчку можно делить или умножать только на число");
-        }
-        for (int i = 0; i < data.length; i++) {
-            data[i] = data[i].replace("\"", "");
-        }
-        if (action == '+') {
-            printInQuotes(data[0] + data[1]);
-
-        } else if (action == '*') {
-            int multiplier = Integer.parseInt(data[1]);
-            String result = "";
-            for (int i = 0; i < multiplier; i++) {
-                result+=data[0];
+public class Mainn {
+public static void main(String[] args) throws Exception {
+System.out.println("Калькулятор умеет выполнять операции сложения строк, \n" +
+    "вычитания строки из строки, умножения строки на число \n" +
+    "и деления строки на число: \"a\" + \"b\", \"a\" - \"b\", \"a\" * b, \"a\" / b.\n" +
+    "Значения строк передаваемых в выражении выделяются двойными кавычками. \n" +
+    "Калькулятор принимает на вход числа от 1 до 10 включительно (не более), и строки длинной не более 10 символов.");
+Scanner scanner = new Scanner(System.in); //запускаю сканер
+String input = scanner.nextLine(); // считываю строку
+String[] value = input.replaceAll("\\s", "").split("\""); //удаляю пробелы и делю введенную строку по кавычкам
+if (!value[0].equals("")) {
+throw new Exception("Первым аргументом выражения должна быть строка");
+}
+if ((value.length != 3) && (value.length != 4)) {
+throw new Exception("т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
+} else {
+String number1 = value[1];
+if (number1.length() > 10) {
+    throw new Exception("Вы ввели длину строки более 10 символов");
+}
+if (value.length == 3) {
+    String number2 = value[2];
+    String number3 = value[2].replaceAll("[+\\-*/]", "");
+    String operation = Operations.getOperator(number2);
+    switch (operation) {
+        case "*":
+            int count = Integer.parseInt(number3); // вторую строку представляю как число
+            if ((count < 1) || (count > 10)) {
+                throw new Exception("Вы ввели число меньше 1 или больше 10");
             }
-
-            if (result.length() > 40){
-                String res = result.substring(0, 40);
-                System.out.println("\""+res+"\"" + "...");
-             } else {
-                printInQuotes(result);
-        }
-
-        } else if (action == '-') {
-            int index = data[0].indexOf(data[1]);
-            if(index == -1){
-                printInQuotes(data[0]);
-            }else{
-                String result = data[0].substring(0, index);
-                result+=data[0].substring(index+data[1].length());
-                printInQuotes(result);
+            String umn = Operations.multiplication(number1, count);
+            if (umn.length() > 40) {
+                System.out.println("\"" + umn.substring(0, 40) + "..." + "\"");
+            } else {
+                System.out.println("\"" + umn + "\"");
             }
-        }else{
-            int newLen = data[0].length()/Integer.parseInt(data[1]);
-            String result = data[0].substring(0,newLen);
-            printInQuotes(result);
-        }
+            break;
+        case "/":
+            int counte = Integer.parseInt(number3); // вторую строку представляю как число
+            if ((counte < 1) || (counte > 10)) {
+                throw new Exception("Вы ввели число меньше 1 или больше 10");
+            }
+            String del = Operations.division(number1, counte);
+            System.out.println("\"" + del + "\"");
     }
-    static void printInQuotes(String text){
-        System.out.println("\""+text+"\"");
+} else {
+    String operation = value[2];
+    String number2 = value[3];
+    if (number2.length() > 10) {
+        throw new Exception("Вы ввели длину строки более 10 символов");
     }
+    switch (operation) {
+        case "+":
+            String sum = Operations.summa(number1, number2);
+            System.out.println("\"" + sum + "\"");
+            break;
+        case "-":
+            if (number1.contains(number2)) {
+                String raz = Operations.difference(number1, number2);
+                System.out.println("\"" + raz + "\"");
+            } else {
+                System.out.println("\"" + number1 + "\"");
+            }
+            break;
+        case "/":
+            throw new Exception("нельзя делить строку на строку ");
+        default:
+            throw new Exception("Строки нельзя умножать и делить (либо Вы ввели неверный знак) ");
+    }
+}
+}
+}
 }
